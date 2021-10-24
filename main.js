2fda792956407407ab3eeed0f456d85ae49ed2d9
Daniel Christopher <91293823+danielchrist-br@users.noreply.github.com>
2021-10-23 23:44:31 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const linksSocialMedia = {
+  github: 'danielchrist-br',
+  youtube: '/channel/UCWlh62TTqQkq-HNdHcZQBiw',
+  facebook: '',
+  instagram: 'o.dani.br',
+  twitter: ''
+}
+
+function changeSocialMedia() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://www.${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMedia()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userPhoto.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
